27d622618573976dad0caca735edc2cac2d45280
JKnochu <chaetsada.k@outlook.com>
2023-10-21 20:29:14 +0700

    add project files

---
+++ main.js
@@ -0,0 +1,10 @@
+function openNav(){
+    document.getElementById('mobile-nav').style.height = "85px";
+    document.getElementById('close-btn').style.display = "block";
+    document.getElementById('overlay').style.display = "block";
+}
+function closeNav(){
+    document.getElementById('mobile-nav').style.height = "0px";
+    document.getElementById('close-btn').style.display = "none";
+    document.getElementById('overlay').style.display = "none";
+}
